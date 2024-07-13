b056bb28abebf4e2577e6d53fe8858bae693c835
AndersonBrasileiro <anderson.dx@gmail.com>
2024-07-13 20:27:24 -0300

    Retirado o autowired

--- src/main/java/com/welcome/welcome/controllers/WelcomeController.java
+++ src/main/java/com/welcome/welcome/controllers/WelcomeController.java
@@ -1,13 +1,11 @@
 package com.welcome.welcome.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	@Autowired
-		
+			
 	@GetMapping ("/welcome")
 	public String buscaar() {
 		return "WelcomeeeeeeTetra!!";
